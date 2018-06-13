1b71bff1a589ce888eb2169d12cb1bafab61f961
Haofu Zhu <haofuzhu@Alyssas-MacBook-Pro-2.local>
2018-06-13 14:11:21 +1000

    Added color change function to progress bubble

--- src/components/ProgressBubble/index.js
+++ src/components/ProgressBubble/index.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 import './style.scss';
 
+const percentageToHue= (percentage, hue0, hue1) => {
+  return (percentage * (hue1 - hue0)) + hue0;
+}
+
 class ProgressBubble extends Component {
 
   render() {
+    const waterColor = `hsla(${percentageToHue(this.props.value / 100, 0, 120)}, 100%, 50%, .7)`;
+    const textColor = `hsla(${percentageToHue(this.props.value / 100, 0, 120)}, 100%, 30%, 1)`;
+
     return (
-      <div className="fu-progress">
+      <div className="fu-progress" style={{border: `5px solid ${textColor}`}}>
         <div className="fu-inner">
-          <div className="fu-percent percent"><span>{this.props.value}</span>%</div>
-          <div className="water" style={{top: 100-this.props.value+'%'}}></div>
+          <div className="water" style={{
+            top: 100-this.props.value+'%',
+            backgroundColor: waterColor
+          }}></div>
+          <div className="fu-percent percent" style={{color: textColor}}><span>{this.props.value}</span>%</div>
           <div className="glare"></div>
         </div>
       </div>
